a51a0e809e618d90087f0f5715478b4d4a195cb1
Kellen Presta <kpresta04@gmail.com>
2021-04-28 17:08:19 +0100

    need to add wrapper

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,7 +6,71 @@
 
     <div class="section">
       <div class="wrapper">
-        <div class="contact-form"></div>
+        <div class="contact">
+          <div class="contact-info">
+            <h3>Let's Get in Touch</h3>
+            <div class="divider bg-color-2"></div>
+            <p class="text-grey">
+              Interested in working with Bethany Marie Design Studio? We'd love
+              to hear from you.
+            </p>
+          </div>
+          <div class="contact-form">
+            <form id="email-form" name="email-form" data-name="Email Form">
+              <div class="side-input-wrapper-50"></div>
+              <div class="side-input-wrapper-50"></div>
+              <div class="form-columns w-row">
+                <div class="w-col w-col-6">
+                  <input
+                    type="text"
+                    class="input w-input"
+                    maxlength="256"
+                    name="Contact-Name-3"
+                    data-name="Contact Name 3"
+                    placeholder="Your name"
+                    id="Contact-Name-3"
+                    required=""
+                  />
+                </div>
+                <div class="w-col w-col-6">
+                  <input
+                    type="email"
+                    class="input w-input"
+                    maxlength="256"
+                    name="Contact-Email-3"
+                    data-name="Contact Email 3"
+                    placeholder="Email address"
+                    id="Contact-Email-3"
+                    required=""
+                  />
+                </div>
+              </div>
+              <input
+                type="text"
+                class="input w-input"
+                maxlength="256"
+                name="Contact-Phone-2"
+                data-name="Contact Phone 2"
+                placeholder="Contact Phone"
+                id="Contact-Phone-2"
+              /><textarea
+                id="Contact-Message-3"
+                name="Contact-Message-3"
+                placeholder="Describe your project..."
+                maxlength="5000"
+                data-name="Contact Message 3"
+                required=""
+                class="input text-area w-input"
+              ></textarea
+              ><input
+                type="submit"
+                value="Submit Message"
+                data-wait="Please wait..."
+                class="button color-2 w-button"
+              />
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -17,7 +81,7 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-.contact-form {
+.contact {
   display: flex;
   margin-right: 15px;
   margin-left: 15px;
@@ -46,7 +110,7 @@ export default {}
 
 h1 {
   position: absolute;
-  top: 30%;
+  top: 35%;
   left: 10%;
   font-style: normal;
   font-weight: bold;
@@ -57,5 +121,32 @@ h1 {
   line-height: 1;
   text-shadow: 1px 2px 4px rgba(0, 0, 0, 0.8);
   margin-bottom: 28px;
+}
+.contact-info {
+  /* display: flex; */
+  width: 33.33%;
+}
+.contact-form {
+  width: 67.77%;
+  display: flex;
+}
+
+.divider {
+  width: 100px;
+  height: 2px;
+  margin-top: 8px;
+  margin-bottom: 24px;
+}
+h3 {
+  margin-top: 0px;
+  margin-bottom: 12px;
+  line-height: 32px;
+  font-weight: 400;
+  text-transform: capitalize;
+}
+p {
+  margin-bottom: 24px;
+  font-size: 14px;
+  line-height: 24px;
 }
 </style>
