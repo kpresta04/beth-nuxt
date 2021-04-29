1e2a1ee87272f964c83a09ebb5ccef42e55b9330
Kellen Presta <kpresta04@gmail.com>
2021-04-29 12:45:51 +0100

    left-col to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="main">
     <Masthead align="right" url="/about-hero.jpg" text="About Me" />
+    <div class="section">
+      <div class="wrapper">
+        <div class="flex-wrapper">
+          <div class="left-col">
+            <h2>
+              <strong><em>Simple</em></strong> &#38; Elegant Design
+            </h2>
+            <div class="divider"></div>
+            <p class="info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates dolorum veritatis voluptatem quia obcaecati dolor autem
+              et sit velit, dolore, veniam numquam cum, perspiciatis nostrum
+              nulla tempore quisquam? Ullam, eligendi!
+            </p>
+            <img src="/simple.jpg" alt="simple design" />
+            <p class="info-text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+              ducimus doloremque quisquam debitis ad eaque iste voluptatem
+              sapiente excepturi repellendus natus ipsa quia quos facilis, amet
+              expedita iusto laboriosam vitae labore harum possimus inventore
+              non eligendi? Pariatur cumque, minus dolores nemo amet voluptatum
+              explicabo illo modi, reprehenderit dolorem id corporis.
+            </p>
+
+            <h4>My Values</h4>
+            <p class="info-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
+              dignissimos omnis magnam deleniti doloremque unde soluta hic modi
+              consectetur fugiat?
+            </p>
+            <ul>
+              <li>Value</li>
+              <li>Value</li>
+              <li>Value</li>
+              <li>Value</li>
+            </ul>
+            <p class="info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              aperiam quidem reiciendis quo cupiditate modi laudantium quia
+              praesentium, quasi autem dolore. Repudiandae sit cumque soluta.
+              Blanditiis doloribus ex commodi. Laudantium!
+            </p>
+          </div>
+          <div class="right-col"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +58,15 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flex-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+.left-col {
+  img {
+    max-width: 540px;
+    height: auto;
+  }
+}
+</style>
