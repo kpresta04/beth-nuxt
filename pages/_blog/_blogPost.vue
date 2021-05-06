873a920f0f9d4196dd78962ff3419f4c05988e3f
Kellen Presta <kpresta04@gmail.com>
2021-05-06 16:38:11 +0100

    f

--- pages/_blog/_blogPost.vue
+++ pages/_blog/_blogPost.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="main"></div>
+  <div class="main">
+    <div class="section">
+      <div class="wrapper">
+        <h1>{{ doc.data.title[0].text }}</h1>
+        <prismic-rich-text :field="doc.data.content" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   async asyncData({ $prismic, params, error }) {
-    // const doc = await $prismic.api.query('')
+    const doc = await $prismic.api.getByID('YJQCwhIAACMAixrc')
+
     console.log(params)
-    // if (doc) {
-    //   return { doc }
-    // } else {
-    //   error({ statusCode: 404, message: 'Page not found' })
-    // }
+    if (doc) {
+      return { doc }
+    } else {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
   },
 }
 </script>
