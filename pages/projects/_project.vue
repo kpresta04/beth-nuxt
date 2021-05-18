c9bd9bd036e04bdc9546adc68df6be84d1cd244d
Kellen Presta <kpresta04@gmail.com>
2021-05-18 15:17:19 +0100

    display project images

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="main">
     <div class="section">
-      <div class="o-label">Project</div>
-      <h1 class="project-name">{{ doc.data.project_name[0].text }}</h1>
+      <div class="project">
+        <div class="flex-wrapper">
+          <div class="o-label">Project</div>
+          <h1 class="project-name">{{ doc.data.project_name[0].text }}</h1>
+          <div
+            class="first-image"
+            :style="`background-image:url(${doc.data.images[0]._image.url});`"
+          ></div>
 
-      <div class="text-wrapper">
-        <prismic-rich-text :field="doc.data.description" />
+          <div class="text-wrapper">
+            <prismic-rich-text :field="doc.data.description" />
+          </div>
+
+          <div class="image-grid">
+            <prismic-image
+              v-for="image in doc.data.images.slice(1, doc.data.images.length)"
+              :field="image._image"
+              :key="image._image.url"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -55,5 +71,37 @@ export default {
 
 .project-name {
   text-transform: capitalize;
+}
+
+.first-image {
+  height: 100vh;
+  width: 100%;
+  max-height: 1100px;
+  max-width: 1400px;
+
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin-bottom: 6.25rem;
+  margin-top: 4rem;
+}
+
+.project {
+  width: 100%;
+  padding-left: 100px;
+}
+.flex-wrapper {
+  width: 100%;
+}
+.image-grid {
+  margin-top: 6.25rem;
+
+  img {
+    margin-bottom: 3.125rem;
+
+    height: 100%;
+    max-height: 100vh;
+    width: auto;
+  }
 }
 </style>
