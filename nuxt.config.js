9c3b6b5bb4642e2525c007cb1f38bd53abb342bf
Kellen Presta <kpresta04@gmail.com>
2021-05-20 13:48:59 +0100

    add description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Bespoke design studio located in south England, UK.',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/sc.svg' },
